a469212d08d0361dc777e9a00e77aa3fe16c6dd5
Skeyling1 <skeyling1@gmail.com>
2025-01-29 17:23:45 +0300

    rrr

--- FlowerDelivery/shop/views.py
+++ FlowerDelivery/shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+
+def registration(request):
+    return render(request, 'shop/registration.html')
+
+
+def cabinet(request):
+    return render(request, 'shop/cabinet.html')
