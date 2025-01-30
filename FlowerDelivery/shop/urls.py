44cc9b65beea7943335c279a3ef7105060f810f0
Skeyling1 <skeyling1@gmail.com>
2025-01-30 15:30:45 +0300

    vv

--- FlowerDelivery/shop/urls.py
+++ FlowerDelivery/shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('reg', views.registration),
-    path('cabinet', views.cabinet),
+    path('cabinet', views.cabinet, name='cabinet'),
 ]
