cd06e33f60fdaa8a304116f5d7ac0de4dfc974a1
Skeyling1 <skeyling1@gmail.com>
2025-01-30 20:01:31 +0300

    bb

--- FlowerDelivery/shop/admin.py
+++ FlowerDelivery/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Goods, Orders
+
+
+admin.site.register(Goods)
+admin.site.register(Orders)
--- FlowerDelivery/shop/models.py
+++ FlowerDelivery/shop/models.py
@@ -1,3 +1,16 @@
+# Модель данных
+# - Таблица пользователей (ID, имя, email).
+# - Таблица товаров (ID, название, цена).
+# - Таблица заказов (ID, пользователь, товары).
+
 from django.db import models
 
-# Create your models here.
+
+class Goods(models.Model):
+	good_title = models.CharField('Название товара', max_length=50)
+	price = models.CharField('Цена', max_length=50)
+
+
+class Orders(models.Model):
+	user = models.CharField('Пользователь', max_length=50)
+	goods = models.CharField('Товары', max_length=50)
