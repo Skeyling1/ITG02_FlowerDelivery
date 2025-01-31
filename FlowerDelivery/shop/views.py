11ceda79f37b6ffb47feb0db7d2d003a8b06b54b
Skeyling1 <skeyling1@gmail.com>
2025-01-31 16:47:05 +0300

    dd

--- FlowerDelivery/shop/views.py
+++ FlowerDelivery/shop/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Good, Order
 
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'shop/index.html')
+    goods = Good.objects.all()
+    username = request.user.username
+    if request.method == 'POST':
+        user_base = request.POST['username']
+        goods_base = request.POST['good_title']
+        new_order = Order(user=user_base, goods=goods_base)
+        new_order.save()
+    return render(request, 'shop/index.html', {'goods': goods, 'username': username})
 
 
 def registration(request):
     return render(request, 'shop/registration.html')
 
 
+@login_required
 def cabinet(request):
-    goods = Good.objects.all()
     orders = Order.objects.all()
-    return render(request, 'shop/cabinet.html', {'goods': goods, 'orders': orders})
+    username = request.user.username
+    return render(request, 'shop/cabinet.html', {'orders': orders, 'username': username})
