72b6b7d4b7c6ab126f9e787d73bf960f6c854781
Skeyling1 <skeyling1@gmail.com>
2025-01-31 20:18:42 +0300

    g

--- FlowerDelivery/shop/urls.py
+++ FlowerDelivery/shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
-    path('reg', views.registration),
+    path('reg/', views.register, name='register'),
     path('cabinet', views.cabinet, name='cabinet'),
 ]
--- FlowerDelivery/shop/views.py
+++ FlowerDelivery/shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Good, Order
 
@@ -16,8 +18,19 @@ def index(request):
     return render(request, 'shop/index.html', {'goods': goods, 'username': username})
 
 
-def registration(request):
-    return render(request, 'shop/registration.html')
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'shop/registration.html', {'form': form})
+
 
 
 @login_required
